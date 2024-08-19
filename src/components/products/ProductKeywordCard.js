330f705418d6f70e6b982a05781d8530328d3fa3
dlswn666 <dlswn666@caelumglobal.com>
2024-08-20 08:58:03 +0900

    Steps, 상품명 등록, 키워드 등록 페이지 완료

---
+++ src/components/products/ProductKeywordCard.js
@@ -0,0 +1,83 @@
+import { Card, Image, Space, Row, Col } from 'antd';
+import React, { useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
+import InputComponent from '../InputComponent';
+import defaultImage from '../../assets/errorImage/20191012_174111.jpg';
+
+const ProductKeywordCard = forwardRef(({ data, isFocused, onCardFocus }, ref) => {
+    const imageSrc = data.images && data.images.length > 0 ? data.images[0] : defaultImage;
+    const inputRef = useRef(null);
+
+    useImperativeHandle(ref, () => ({
+        focusInput: () => {
+            if (inputRef.current) {
+                inputRef.current.focus();
+            }
+        },
+    }));
+
+    useEffect(() => {
+        console.log(isFocused);
+        if (isFocused && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
+
+    const fnOnBlur = () => {
+        console.log('데이터 저장');
+    };
+
+    return (
+        <Space direction="vertical" size="middle" style={{ display: 'block', width: '100%' }}>
+            <Card hoverable style={{ width: '100%' }} onFocus={onCardFocus} tabIndex={0}>
+                <Image.PreviewGroup items={data.images && data.images.length > 0 ? data.images : [defaultImage]}>
+                    <div style={{ display: 'flex', flex: 1 }}>
+                        <Image width={150} src={imageSrc} fallback={defaultImage} alt="Product Image" />
+                        <div style={{ marginLeft: 16, flex: 1 }}>
+                            <Row gutter={[4, 1]}>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 16, fontWeight: 'bold' }}>상품 이름 :</p>
+                                </Col>
+                                <Col span={20}>
+                                    <p style={{ fontSize: 14 }}>{data.productTitle}</p>
+                                </Col>
+                            </Row>
+                            <Row gutter={[4, 1]}>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 16, fontWeight: 'bold' }}>판매 사이트 :</p>
+                                </Col>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 14 }}>{data.siteName}</p>
+                                </Col>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 16, fontWeight: 'bold' }}>상품 번호 :</p>
+                                </Col>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 14 }}>{data.productNumber}</p>
+                                </Col>
+                            </Row>
+                            <Row gutter={[4, 1]}>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 16, fontWeight: 'bold' }}>판매 가격 :</p>
+                                </Col>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 14 }}>{data.price}</p>
+                                </Col>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 16, fontWeight: 'bold' }}>설정 검색어 :</p>
+                                </Col>
+                                <Col span={4}>
+                                    <p style={{ fontSize: 14 }}>{data.productNumber}</p>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                </Image.PreviewGroup>
+                <div style={{ marginTop: 16 }}>
+                    <InputComponent ref={inputRef} placeholder="상품 검색 키워드를 입력해주세요" onBlur={fnOnBlur} />
+                </div>
+            </Card>
+        </Space>
+    );
+});
+
+export default ProductKeywordCard;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Space, Row, Col, Ste } from 'antd';
+import { Space, Row, Col, Steps } from 'antd';
 import SearchKeywordCard from '../components/products/SearchKeywordCard';
+import ProductNameCard from '../components/products/ProductNameCard';
+//검색어 등록 데이터
 import testData from '../assets/testData/test';
+import productNameTestData from '../assets/testData/productNameTestData';
+import ProductKeywordCard from '../components/products/ProductKeywordCard';
 
 const Products = () => {
     const initImageGroup = [
@@ -10,58 +14,235 @@ const Products = () => {
         'https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp',
     ];
 
+    // 검색어 등록 데이터
     const modifiedTestData = testData.map((item) => ({
         ...item,
         images: initImageGroup,
     }));
 
-    const [focusedIndex, setFocusedIndex] = useState(0);
-    const cardRefs = useRef([]);
+    const modifiedProductTestData = productNameTestData.map((item) => ({
+        ...item,
+        images: initImageGroup,
+    }));
 
-    const handleKeyDown = (e) => {
+    // 각 컴포넌트별 포커스 상태와 참조 관리
+    const [searchKeywordFocusedIndex, setSearchKeywordFocusedIndex] = useState(0);
+    const searchKeywordCardRefs = useRef([]);
+
+    const [productNameFocusedIndex, setProductNameFocusedIndex] = useState(0);
+    const productNameCardRefs = useRef([]);
+
+    const [productKeywordFocusedIndex, setProductKeywordFocusedIndex] = useState(0);
+    const productKeywordCardRefs = useRef([]);
+
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const handleSearchKeywordKeyDown = (e) => {
         if (e.key === 'ArrowDown') {
-            setFocusedIndex((prevIndex) => {
+            setSearchKeywordFocusedIndex((prevIndex) => {
                 const newIndex = Math.min(prevIndex + 1, modifiedTestData.length - 1);
-                cardRefs.current[newIndex]?.focusInput(); // input에 포커스 이동
+                searchKeywordCardRefs.current[newIndex]?.focusInput();
                 return newIndex;
             });
         } else if (e.key === 'ArrowUp') {
-            setFocusedIndex((prevIndex) => {
+            setSearchKeywordFocusedIndex((prevIndex) => {
                 const newIndex = Math.max(prevIndex - 1, 0);
-                cardRefs.current[newIndex]?.focusInput(); // input에 포커스 이동
+                searchKeywordCardRefs.current[newIndex]?.focusInput();
+                return newIndex;
+            });
+        }
+    };
+
+    const handleProductNameKeyDown = (e) => {
+        if (e.key === 'ArrowDown') {
+            setProductNameFocusedIndex((prevIndex) => {
+                const newIndex = Math.min(prevIndex + 1, modifiedProductTestData.length - 1);
+                productNameCardRefs.current[newIndex]?.focusInput();
+                return newIndex;
+            });
+        } else if (e.key === 'ArrowUp') {
+            setProductNameFocusedIndex((prevIndex) => {
+                const newIndex = Math.max(prevIndex - 1, 0);
+                productNameCardRefs.current[newIndex]?.focusInput();
+                return newIndex;
+            });
+        }
+    };
+
+    const handleProductKeywordKeyDown = (e) => {
+        if (e.key === 'ArrowDown') {
+            setProductKeywordFocusedIndex((prevIndex) => {
+                const newIndex = Math.min(prevIndex + 1, modifiedProductTestData.length - 1);
+                productKeywordCardRefs.current[newIndex]?.focusInput();
+                return newIndex;
+            });
+        } else if (e.key === 'ArrowUp') {
+            setProductKeywordFocusedIndex((prevIndex) => {
+                const newIndex = Math.max(prevIndex - 1, 0);
+                productKeywordCardRefs.current[newIndex]?.focusInput();
                 return newIndex;
             });
         }
     };
 
     useEffect(() => {
-        // 페이지 진입 시 첫 번째 카드의 input에 포커스
-        console.log('실행1');
-        cardRefs.current[0]?.focusInput();
-    }, []);
+        // 각 스텝에 처음 진입할 때 첫 번째 카드에 포커스
+        if (currentStep === 0) {
+            searchKeywordCardRefs.current[0]?.focusInput();
+        } else if (currentStep === 1) {
+            productNameCardRefs.current[0]?.focusInput();
+        }
+    }, [currentStep]);
+
+    // 각 스텝의 활성화 상태를 관리할 수 있는 상태 변수 (예시로 두 번째 스텝을 비활성화)
+    const [stepDisabledStatus, setStepDisabledStatus] = useState([
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+    ]);
+
+    const steps = [
+        {
+            title: '검색어 등록',
+            description: '검색어를 입력하세요',
+            content: (
+                <Row>
+                    <Col span={24}>
+                        <Space
+                            direction="vertical"
+                            size={'middle'}
+                            style={{ display: 'flex' }}
+                            onKeyDown={handleSearchKeywordKeyDown}
+                            tabIndex={0}
+                        >
+                            {modifiedTestData.map((item, index) => (
+                                <SearchKeywordCard
+                                    key={index}
+                                    data={item}
+                                    isFocused={index === searchKeywordFocusedIndex}
+                                    ref={(el) => (searchKeywordCardRefs.current[index] = el)}
+                                    onCardFocus={() => setSearchKeywordFocusedIndex(index)}
+                                />
+                            ))}
+                        </Space>
+                    </Col>
+                </Row>
+            ),
+        },
+        {
+            title: '상품명 등록',
+            description: '상품명을 입력하세요',
+            content: (
+                <Row>
+                    <Col span={24}>
+                        <Space
+                            direction="vertical"
+                            size={'middle'}
+                            style={{ display: 'flex' }}
+                            onKeyDown={handleProductNameKeyDown}
+                            tabIndex={0}
+                        >
+                            {modifiedProductTestData.map((item, index) => (
+                                <ProductNameCard
+                                    key={index}
+                                    data={item}
+                                    isFocused={index === productNameFocusedIndex}
+                                    ref={(el) => (productNameCardRefs.current[index] = el)}
+                                    onCardFocus={() => setProductNameFocusedIndex(index)}
+                                />
+                            ))}
+                        </Space>
+                    </Col>
+                </Row>
+            ),
+        },
+        {
+            title: '키워드 등록',
+            description: '상품 키워드을 입력하세요',
+            content: (
+                <Row>
+                    <Col span={24}>
+                        <Space
+                            direction="vertical"
+                            size={'middle'}
+                            style={{ display: 'flex' }}
+                            onKeyDown={handleProductKeywordKeyDown}
+                            tabIndex={0}
+                        >
+                            {modifiedProductTestData.map((item, index) => (
+                                <ProductKeywordCard
+                                    key={index}
+                                    data={item}
+                                    isFocused={index === productKeywordFocusedIndex}
+                                    ref={(el) => (productKeywordCardRefs.current[index] = el)}
+                                    onCardFocus={() => setProductKeywordFocusedIndex(index)}
+                                />
+                            ))}
+                        </Space>
+                    </Col>
+                </Row>
+            ),
+        },
+        {
+            title: '태그 등록',
+            description: '상품명 등록',
+            content: <div>상품 선택 컴포넌트 내용</div>,
+        },
+        {
+            title: '카테고리 등록',
+            description: '상품명 등록',
+            content: <div>상품 선택 컴포넌트 내용</div>,
+        },
+        {
+            title: '가격 설정',
+            description: '상품명 등록',
+            content: <div>상품 선택 컴포넌트 내용</div>,
+        },
+        {
+            title: '상품 옵션 설정',
+            description: '상품명 등록',
+            content: <div>상품 선택 컴포넌트 내용</div>,
+        },
+        {
+            title: '썸네일 가공',
+            description: '상품명 등록',
+            content: <div>상품 선택 컴포넌트 내용</div>,
+        },
+        {
+            title: '최종 확인',
+            content: <div>선택한 상품을 확인하는 컴포넌트 내용</div>,
+        },
+        // 다른 스텝들도 추가 가능
+    ];
 
     return (
         <>
-            <Row>
+            <Row style={{ margin: '30px 0' }}>
                 <Col span={24}>
-                    <Space
-                        direction="vertical"
-                        size={'middle'}
-                        style={{ display: 'flex' }}
-                        onKeyDown={handleKeyDown}
-                        tabIndex={0}
-                    >
-                        {modifiedTestData.map((item, index) => (
-                            <SearchKeywordCard
-                                key={index}
-                                data={item}
-                                isFocused={index === focusedIndex}
-                                ref={(el) => (cardRefs.current[index] = el)} // 카드에 대한 참조 저장
-                                onCardFocus={() => setFocusedIndex(index)}
-                            />
-                        ))}
-                    </Space>
+                    <Steps
+                        current={currentStep}
+                        onChange={(newStep) => {
+                            // 클릭된 스텝이 비활성화 상태가 아니면 스텝을 변경
+                            if (!stepDisabledStatus[newStep]) {
+                                setCurrentStep(newStep);
+                            }
+                        }}
+                        items={steps.map((step, index) => ({
+                            title: step.title,
+                            description: step.description,
+                            disabled: stepDisabledStatus[index], // 각 스텝의 비활성화 상태 적용
+                        }))}
+                    />
                 </Col>
+            </Row>
+            <Row style={{ marginTop: 24 }}>
+                <Col span={24}>{steps[currentStep].content}</Col>
             </Row>
         </>
     );
